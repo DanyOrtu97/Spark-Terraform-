90cbca201b52393db27144fe5fdb325b657b6151
Daniele Ortu <37103272+DanyOrtu97@users.noreply.github.com>
2021-02-19 22:49:35 +0100

    Updated

--- variables.tf
+++ variables.tf
@@ -5,17 +5,17 @@ variable "region" {
 
 variable "access_key" {
     type = string
-    default = "ASIA3L6MJAFSSEDNTDG3"
+    default = " "
 }
 
 variable "secret_key" {
     type = string
-    default = "GOWjBY0LR53FOqzIapG+VCADMIlfTZNX3wyYelwF"
+    default = " "
 }
 
 variable "token" {
     type = string
-    default = "FwoGZXIvYXdzEJf//////////wEaDOoJDsZgLi+mBHSXxSLMAQq9XG9nuTP4bkBVX5yZujsc7xQZu5cUFP+C0nDrEHwlkQeV3p/expI15Bb/x9Mz3yDfC2M60o89uthqD1RpMDzBZa14maArwd+LXr4xyidnaFIFzwbMFass1ATIV/ON6ybriy42I9jLylq9HZBs5PA7WcdUGDLec42kYJ5fxbqCDJv0NUosOJ7tcYRx8oxo++E+uArPfxPCop4vhrCsB3ny1Cwi6132aPe07H1P7RfP701RHTD2nT7lEBLtJkcDbESigIvFxocDoMioaSj8iL+BBjItdny910n3xdjVq6SolfKjWIsTy8JuHTGAT/75JBtRn2Iy4PLSli46afZ09LiV"
+    default = " "
 }
 
 variable "instance_type" {
